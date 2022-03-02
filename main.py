a396452dd745bff6184090c259e78423d0eafaba
changeAJay <ajajay008@gmail.com>
2022-03-02 18:22:14 +0530

    Updated paths of txt documents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 #for each name in invited_names.txt
-with open("D:/python/Mail Merge Project Start/Input/Names/invited_names.txt") as data:
+with open("./Input/Names/invited_names.txt") as data:
     data = data.readlines()
 
 names = []
@@ -11,12 +11,12 @@ for n in data:
 
 #Replace the [name] placeholder with the actual name.
 for n in names:
-    with open("D:/python/Mail Merge Project Start/Input/Letters/starting_letter.txt") as data:
+    with open("./Input/Letters/starting_letter.txt") as data:
         placeholder = data.read()
         x = placeholder.replace('[name]', n)
 
 #Save the letters in the folder "ReadyToSend".
-    with open(f"D:/python/Mail Merge Project Start/Output/ReadyToSend/letter_for_{n}.txt", mode='w') as data:
+    with open(f"./Output/ReadyToSend/letter_for_{n}.txt", mode='w') as data:
         data.write(x)
 
 
